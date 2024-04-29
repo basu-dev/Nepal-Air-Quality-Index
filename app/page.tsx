f955cff04a7c2fba72a6644afda12ea4710a6934
Basu Dev Adhikari <adbasudev54@gmail.com>
2024-04-29 20:31:18 +0545

    min height added for map & header made darker

--- app/page.tsx
+++ app/page.tsx
@@ -22,12 +22,12 @@ const [data,nepalGeoJson]= await Promise.all([getAirQualityData(),getNepalGeoJso
 
   return (
     <main className="p-2 flex flex-col h-[100dvh]">
-      <header className="text-gray-700 font-bold text-xl border-b-2 my-3 pb-2">Nepal Air Quality Index </header>
+      <header className="text-gray-800 font-bold text-xl border-b-2 my-3 pb-2">Nepal Air Quality Index </header>
       <div className="flex flex-col md:flex-row gap-2 h-full">
-        <div className="flex-1">
+        <div className="min-h-[300px] flex-1">
           <DynamicMap data={filteredDta} nepalGeoJson={nepalGeoJson} />
         </div>
-      <MapSummary data={filteredDta}/> 
+        <MapSummary data={filteredDta}/> 
       </div>
       <footer className="fixed bottom-1 right-2 text-xs">
         All the data are taken from <a className="text-blue-700" target="_blank" href="https://waqi.info">waqi.info</a>
